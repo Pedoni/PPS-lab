b505d4484219f6af5e687f45fab2f26fca7330ce
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2022-03-08 12:58:24 +0100

    modifiche lab02

--- lab02/src/test/scala/u02/Es6.scala
+++ lab02/src/test/scala/u02/Es6.scala
@@ -9,8 +9,18 @@ class Es6 {
     case 0 | 1 => x
     case x => fib(x-1) + fib(x-2)
 
+  def fibTail(x: Int): Int =
+    @annotation.tailrec
+    def tail(x: Int, prev: Int, curr: Int): Int = (x<=0) match
+      case true => curr
+      case _ => tail(x - 1, prev + curr, prev)
+    tail(x, prev = 1, curr = 0)
+
   
   @Test def testFibonacci() =
+    assertEquals(fib(7), 13)
+
+  @Test def testFibonacciTail() =
     assertEquals(fib(7), 13)
   
 }
